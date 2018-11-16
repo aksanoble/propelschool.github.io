5c6485cdd1e704e3bb67ecc9ceb643e15553ea9e
Sachin M K <sachinmk27@gmail.com>
2018-11-16 16:37:40 +0530

    1. Remove git tracking for public folder 2. Update UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,32 @@
-import React from 'react'
+import React from 'react';
 
-import 'bulma/css/bulma.css'
-import './index.css'
+import 'bulma/css/bulma.css';
+import './index.css';
 
 const IndexPage = () => (
   <React.Fragment>
     <section className="hero is-link is-fullheight is-bold">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">Welcome to Propel.</h1>
-          <h1 className="title">
+          <h1 className="is-size-1 has-text-weight-bold">Welcome to Propel</h1>
+          <h1 className="is-size-3">
             A Revolutionary New School
             <br />
             That Invests in You.
           </h1>
-          <h2 className="subtitle">
+          <p className="content is-medium ">
             Currently offering a 2-week immersive program to launch your
-            software engineering career. <strong>For free.</strong><br/>
-            Our next program starts from <strong>19th November, 2018</strong>.
-            Apply here <a href="https://goo.gl/forms/D5qSHdZu6pMDRqJ13">https://goo.gl/forms/D5qSHdZu6pMDRqJ13</a>.
-          </h2>
+            software engineering career.
+            <span className="has-text-weight-bold"> For free.</span>
+            <br />
+            Our next program starts from
+            <span className="has-text-weight-bold"> 19th November, 2018.</span>
+          </p>
+          <p><a className="button is-warning" target="_blank" rel="noopener noreferrer" href="https://goo.gl/forms/D5qSHdZu6pMDRqJ13">Apply Now</a></p>
         </div>
       </div>
     </section>
   </React.Fragment>
-)
+);
 
-export default IndexPage
+export default IndexPage;
