c84d7cce93f270a2d6131eb771eede3dc0d89546
Sachin M K <sachinmk27@gmail.com>
2018-11-17 18:14:17 +0530

    Updates

---
+++ 10-fb1f7e4a74de99ac8a8b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{163:function(n,w,o){}}]);
+//# sourceMappingURL=10-fb1f7e4a74de99ac8a8b.js.map
--- sw.js
+++ sw.js
@@ -11,8 +11,8 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("workbox-v3.6.2/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "workbox-v3.6.2"});
+importScripts("workbox-v3.5.0/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.5.0"});
 
 workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
 
@@ -26,27 +26,27 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b00993d9591163cbd30b.js"
+    "url": "webpack-runtime-3526967a2deb8ef1469c.js"
   },
   {
-    "url": "app-ee5df98044faf10e5620.js"
+    "url": "app-4c0e174c84bfbe23274d.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-0691004cd9de246c1542.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-9c49c867da16740cb193.js"
   },
   {
     "url": "index.html",
-    "revision": "cd30383aecb2fcfce1412fbdae631a02"
+    "revision": "17df4e1f99e912ca04b0fedb9f7fd858"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3192cc002fac57909f164c6cffe977b8"
+    "revision": "734f77f19a08f4ddf7d1e535a19ffcaf"
   },
   {
-    "url": "10.b677124a83a75393affb.css"
+    "url": "10.fc16002c9c09342a9a25.css"
   },
   {
-    "url": "component---src-pages-index-js.0f8f5cb05a342fb8dee7.css"
+    "url": "component---src-pages-index-js.9502314f6da9d89234d2.css"
   },
   {
     "url": "google-fonts/s/opensans/v15/mem5YaGs126MiZpBA-UN7rgOUuhp.woff2",
@@ -57,20 +57,20 @@ self.__precacheManifest = [
     "revision": "cffb686d7d2f4682df8342bd4d276e09"
   },
   {
-    "url": "component---src-pages-index-js-c4cac1401a8cbaf37be5.js"
+    "url": "component---src-pages-index-js-7793da6a182be8d91555.js"
   },
   {
-    "url": "10-ec203a27c5f4d8aca178.js"
+    "url": "10-fb1f7e4a74de99ac8a8b.js"
   },
   {
     "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
     "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
   },
   {
-    "url": "0-40fda80323a334a7dce3.js"
+    "url": "0-1ca2b496ae155a2ac34c.js"
   },
   {
-    "url": "component---src-pages-404-js-53535ac789106d0a9ba4.js"
+    "url": "component---src-pages-404-js-0f80cf25fcb6db7abbf0.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
@@ -82,7 +82,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "55a9292126f8836e91e5d40b0a8cb54f"
+    "revision": "cbac88ea51ba22f4993bc21f9fb725e1"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
