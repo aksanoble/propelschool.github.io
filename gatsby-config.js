82118bfc72317d30c48aa9a0939b620101c9ac3c
Sachin M K <sachinmk27@gmail.com>
2018-11-19 22:19:11 +0530

    Add google analytics support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,28 +5,48 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: 'hsl(217, 71%, 53%)',
+        theme_color: 'hsl(217, 71%, 53%)',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-offline',
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
-            family: `Open Sans`,
-            variants: [`400`, `700`]
+            family: 'Open Sans',
+            variants: ['400', '700'],
           },
         ],
       },
-    }
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-129363470-1',
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: [],
+        // Enables Google Optimize using your container Id
+        // optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
+        // Any additional create only fields (optional)
+        // sampleRate: 5,
+        // siteSpeedSampleRate: 10,
+        // cookieDomain: 'propel.school',
+      },
+    },
   ],
-}
+};
